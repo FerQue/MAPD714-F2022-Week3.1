481a291a4ce3aa1c9a219ac32f35375a5cbff189
Hisham Sanimeh <h.snaimeh93@gmail.com>
2022-09-25 23:31:54 -0400

    cahnge the design

--- MAPD714-F2022-Week3/MAPD714-F2022-Week3/ViewController.swift
+++ MAPD714-F2022-Week3/MAPD714-F2022-Week3/ViewController.swift
@@ -1,5 +1,13 @@
 import UIKit
 
+
+// Internal Documentation
+// File Name: MAPD714-F2022-Assignment 1 - Calculator App - Part 1 - Create the app UI
+// Author's name: Fernando Quezada - 301286477, Hisham Abu Sanimeh - 301289364
+// App Description: This app performs mathematical operations such as addition,
+// substraction, multiplication, and division.
+// Version Information: version 1
+
 class ViewController: UIViewController {
 
     //Result Label
@@ -21,10 +29,9 @@ class ViewController: UIViewController {
     @IBAction func NumberButton_Pressed(_ sender: UIButton)
     {
         var button = sender as UIButton
-        ResultLabel.text = button.titleLabel!.text
+//        resultLabel.text = button.titleLabel!.text
         
-        
-        
+
     }
     
     
